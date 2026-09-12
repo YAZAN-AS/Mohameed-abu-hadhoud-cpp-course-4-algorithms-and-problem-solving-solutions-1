8cce363c577da659394456d5703438458188cb0a
YAZAN.AS <samer.asma92@gmail.com>
2026-09-12 13:07:07 +0300

    Add new subfolder with file

---
+++ P31 Power of 2,3,4/P31 Power of 2,3,4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int N;
+	cout << "Please enter anumber: ";
+	cin >> N;
+	cout << endl << N * N << endl << N * N * N << endl << N * N * N * N << endl;
+	system("pause>0");
+	return 0;
+
+
+}
